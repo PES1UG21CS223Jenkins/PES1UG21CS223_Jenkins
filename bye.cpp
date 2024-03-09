aacae163387079bd530093b8820eb281c21dc545
PES1UG21CS223Jenkins <162812918+PES1UG21CS223Jenkins@users.noreply.github.com>
2024-03-09 21:19:16 +0530

    bye.cpp

---
+++ bye.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Bye, World\n";
+  cout << "Bye, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PES1UG21CS223\n";
+  return 0;
+}
